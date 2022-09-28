6c17fe9f6f03af2269b3dd836cb47e36451c7921
nishiyama <mickey-ya@outlook.jp>
2022-09-29 01:15:34 +0900

    fix: base dio

--- lib/src/data/remote/base_dio.dart
+++ lib/src/data/remote/base_dio.dart
@@ -4,10 +4,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final dioProvider = Provider<Dio>((_) => BaseDio.getInstance());
 
+typedef QueriesMap = Map<String, dynamic>;
+
 class BaseDio with DioMixin implements Dio {
   BaseDio._([BaseOptions? options]) {
     options = BaseOptions(
-      baseUrl: 'http://localhost:8080',
+      baseUrl:
+          'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404',
       contentType: 'application/json',
       receiveDataWhenStatusError: true,
       connectTimeout: 60 * 1000,
